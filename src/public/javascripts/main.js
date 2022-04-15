ccce34530fc0d6edc83409950de13628a036b9ee
Jennifer Tang <jt3744@nyu.edu>
2022-04-15 03:09:35 -0400

    part 3 done

--- src/public/javascripts/main.js
+++ src/public/javascripts/main.js
@@ -41,11 +41,6 @@ function getDeck(splitStart){
 			deck.push(card);
 		}
 	}
-	
-  
-
-
-
 /* console.log(splitStart);
 check = deck.filter(function(objFromA) {
   return !splitStart.find(function(objFromB) {
@@ -78,6 +73,31 @@ function firstDeal(deck, splitStart){
   Dealer.push(deck.shift());
   Player.push(deck.shift());
 
+
+
+
+  const playerTotal = elt('div','playerTotal',`Player hand - Total: ${handTotal(Player)}`);
+  const dealerTotal = elt('div','dealerTotal','Computer Hand - Total: ?');
+  const dealerCards = elt('div', 'dealerCards');
+  const playerCards = elt('div', 'playerCards');
+
+  const choice = elt('div', 'choice', elt('button','hit', 'Hit'), elt('button','hit', 'Stand'));
+  //document.body.appendChild(ul);
+  let game = document.querySelector('.game');
+  
+  game.appendChild(dealerTotal);
+  game.appendChild(dealerCards);
+  game.appendChild(playerTotal);
+  game.appendChild(playerCards);
+  game.appendChild(choice);
+
+  makeCard(Dealer[0], '.dealerCards');
+  let dealerHidden = document.querySelector('.dealerCards .card');
+  dealerHidden.className = 'hiddenCard';
+  makeCard(Player[0], '.playerCards');
+  makeCard(Dealer[1], '.dealerCards');
+  makeCard(Player[1], '.playerCards');
+
 /* let i = 0;
    while(i < length){
     if(i%2 === 0){
@@ -92,7 +112,56 @@ function firstDeal(deck, splitStart){
     }
 
   } */
-  //console.log(Dealer);
   return [Player, Dealer];
   
+}
+
+function makeCard(card, side) {
+
+  const e = elt('div', 'card', `${card.Value} ${card.Suit}`);
+  const check = document.querySelector(side);
+  check.appendChild(e);
+}
+
+
+function handTotal(side) {
+  side = side.map(x => ['J', 'Q', 'K'].includes(x.Value) ? 10 : x.Value);
+  let aces = 0;
+  for(let i=0;i<side.length;i++) {
+    if(side[i] === 'A') {
+      aces++;
+    }
+  }
+  side = side.filter(x => x === 'A' ? '' : x);
+  side = parseInt(side.reduce((prev, cur) => parseInt(prev) + parseInt(cur), 0));
+  for(let i=aces;i>0;i--) {
+    side = aces ? (side + 11 > 21 ? side + 1 : side + 11) : side; 
+  }
+  console.log(side);
+  return side;
+}
+
+
+function stand(deck, Player, Dealer) {
+  document.querySelector('.hidden').classList.remove('hidden');
+  document.querySelector('.hitStand').style.display = 'none';
+  document.querySelector('.dealerTotal').innerText = `Computer Hand - Total: ${handTotal(dealer)}`;
+  while (handTotal(dealer) <= 16) {
+    deal(deck, '.dealerCards', Player, Dealer);
+  }
+  checkWinner(Player, Dealer);
+}
+
+function elt(type) {
+	const ele = document.createElement(type);
+  ele.className = arguments[1];
+	// start at 2 or else we'll get the type argument!
+	for (let i = 2; i < arguments.length; i++) {
+		let child = arguments[i];
+		if (typeof child === "string") {
+			child = document.createTextNode(child);
+		}
+		ele.appendChild(child);
+	}
+	return ele;
 }
